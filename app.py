64db4fda342c6b670b6b66d83f6c139327e8cf48
kuzi117 <braedy@ualberta.ca>
2015-12-14 22:48:50 -0500

    Basic index rendering.

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
+from flask import Flask  
+from flask import render_template  
+from flask import request
+
 from challonge import *
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()

a6d67f2be3bf41943dd6083fabb3197827c6bb3b
Elliot Colp <thelastbanana@gmail.com>
2015-12-19 15:41:11 -0500

    Add simple 404 page

--- app.py
+++ app.py
@@ -15,6 +15,10 @@ challonge.set_credentials(os.environ.get('BB_CHALLONGE_USER'), os.environ.get('B
 
 app = Flask(__name__)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 @app.route('/')
 @app.route('/index')
 def index():

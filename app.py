862dee9fab3e671fba89b1cf19ed3162333a6581
Elliot Colp <thelastbanana@gmail.com>
2015-12-18 21:28:07 -0500

    Added a user login page with basic participant retrieval

--- app.py
+++ app.py
@@ -1,15 +1,44 @@
 from flask import Flask  
 from flask import render_template  
 from flask import request
+from flask import abort
+from bidict import bidict
 
-from challonge import *
+import os
+import challonge
+
+# Bi-directional map from tournament's Challonge URL suffix to its ID.
+# We'll actually build this from the Challonge API later.
+tourneys = bidict({ 'thelastbanana_test': '2119181' })
+
+challonge.set_credentials(os.environ.get('BB_CHALLONGE_USER'), os.environ.get('BB_CHALLONGE_API_KEY'))
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
+
+# Login in a tourney participant
+@app.route('/login/<tourneyName>')             
+def user_login(tourneyName):
+    if tourneyName not in tourneys:
+        abort(404)
+
+    tourneyId = tourneys[tourneyName]
+    tourneyData = challonge.tournaments.show(tourneyId)
+    prettyName = tourneyData['name']
+    
+    participants = challonge.participants.index(tourneyId)
+    participantNames = [ participant['name'] for participant in participants ]
+    
+    # Boolean value is whether the user is already logged in
+    participantPairs = [(name, False) for name in participantNames]
+
+    return render_template('user-login.html',
+                           tourneyName=prettyName,
+                           participants=participantPairs)
 
 if __name__ == '__main__':
     app.debug = True

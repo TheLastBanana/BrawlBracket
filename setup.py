49e26309e7b0fb8d30824ebc2e4269ff4b238503
Elliot Colp <thelastbanana@gmail.com>
2015-12-19 21:48:19 -0500

    Update requirements and setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from setuptools import setup
 
-setup(name='BrawlBracket',
-      version='0.1',
-      description='A web app to simplify running Brawlhalla tournaments through Challonge!',
-      author='Elliot Colp and Braedy Kuzma'
-     )
+setup(
+    name='BrawlBracket',
+    version='0.1',
+    description='A web app to simplify running Brawlhalla tournaments through Challonge!',
+    author='Elliot Colp and Braedy Kuzma',
+    install_requires=[
+        'python-dateutil==2.2',
+        'bidict==0.9.0.post1',
+        'Flask==0.10.1',
+        'Flask-Login==0.3.2'
+    ]
+)

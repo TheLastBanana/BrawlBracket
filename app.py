4ff408b1da0cdc5201e66ee24e9e775d328d1588
kuzi117 <braedy@ualberta.ca>
2016-01-03 01:14:58 -0500

    Place sockets into rooms based on match id.

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from flask import url_for
 from flask import session
 from flask import g
 from flask_socketio import SocketIO
+from flask_socketio import join_room, leave_room
 from bidict import bidict
 
 import os
@@ -16,6 +17,7 @@ import brawlapi
 # Bi-directional map from tournament's Challonge URL suffix to its ID.
 # We'll actually build this from the Challonge API later.
 tourneys = bidict({ 'thelastbanana_test': '2119181' })
+tourneyDatas = {}
 
 # Set of online participant IDs
 onlineUsers = set()
@@ -44,9 +46,18 @@ def user_login(tourneyName):
     if tourneyName not in tourneys:
         abort(404)
 
+    tourneyId = tourneys[tourneyName]
+    
     if request.method == 'GET':
-        tourneyId = tourneys[tourneyName]
-        tourneyData = challonge.tournaments.show(tourneyId)
+        
+        # This should request from the database after checking the memory cache
+        # and before going to Challonge
+        if tourneyId not in tourneyDatas:
+            tourneyData = challonge.tournaments.show(tourneyId)
+            tourneyDatas[tourneyId] = tourneyData
+        else:
+            tourneyData = tourneyDatas[tourneyId]
+        
         prettyName = tourneyData['name']
         tourneyURL = tourneyData['full-challonge-url']
         
@@ -63,6 +74,7 @@ def user_login(tourneyName):
     # TODO: validate user ID
     userId = request.form['user']
     session['participantId'] = userId
+    session['tourneyId'] = tourneyId
     
     return redirect(url_for('user_landing', tourneyName=tourneyName))
 
@@ -81,18 +93,40 @@ def lobby():
 @socketio.on('connect', namespace='/participant')
 def participant_connect():
     pId = session.get('participantId')
+    tId = session.get('tourneyId')
     
     if not pId:
         print('Participant id missing; connection rejected')
         return False
     
     if pId in onlineUsers:
-        print('Participant #{} rejected'.format(pId))
+        print('Participant #{} rejected (already connected)'.format(pId))
         return False
         
     onlineUsers.add(pId)
     
-    print('Participant #{} connected'.format(pId))
+    # Leave old room
+    if 'matchId' in session:
+        print('Participant #{} connecting, left old room #{}'
+            .format(pId, session['matchId']))
+        leave_room(str(session['matchId']))
+    
+    # Find current match
+    matchesData = challonge.matches.index(tId)
+    matchData = None
+    for match in matchesData:
+        print(pId, match['player1-id'], match['player2-id'], type(match['winner-id']))
+        if int(pId) in [match['player1-id'], match['player2-id']] \
+                and match['winner-id'] is None:
+            matchData = match
+            break
+    
+    # Join new room
+    session['matchId'] = matchData['id']
+    join_room(str(matchData['id']))
+    
+    print('Participant #{} connected, joined room #{}'
+        .format(pId, matchData['id']))
     
 @socketio.on('disconnect', namespace='/participant')
 def participant_disconnect():

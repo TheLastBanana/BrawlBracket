847b3a0f446c872ff0b5bfc3b1ccd9011591c806
Elliot Colp <thelastbanana@gmail.com>
2016-02-10 23:07:19 -0500

    Added sorting that ignores empty player names

--- static/brawlbracket/js/admin-dashboard.js
+++ static/brawlbracket/js/admin-dashboard.js
@@ -1,3 +1,24 @@
+// Custom sorting function to put empty elements at the bottom
+$.extend($.fn.dataTableExt.oSort, {
+    'anti-empty-asc': function (a, b) {
+        if (a == '') return 1;
+        if (b == '') return -1;
+        
+        return a.localeCompare(b);
+    },
+  
+    'anti-empty-desc': function (a, b) {
+        if (a == '') return 1;
+        if (b == '') return -1;
+        
+        return b.localeCompare(a);
+    }
+});
+
 $(function() {
-    $('#bb-match-table').DataTable();
+    $('#bb-match-table').DataTable({
+        'columnDefs': [
+            { 'type': 'anti-empty', 'targets': [1, 2] }
+        ]
+    });
 });

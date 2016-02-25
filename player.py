7fd24c3dd9da12f0c64de27b7e6cf60ef4ecf548
kuzi117 <braedy@ualberta.ca>
2016-02-24 21:02:21 -0500

    Add Player. Closes #23.

---
+++ player.py
@@ -0,0 +1,21 @@
+class Player:
+    """
+    A Player on a Team in a Tournament. This is linked to a single team in a
+    single tournament.
+    """
+    def __init__(uuid, user):
+        """
+        Player data:
+         id: BrawlBracket id (uuid)
+         user: BrawlBracket user (User)
+        
+        Tournament data:
+         currentLegend: currently selected legend (string id)
+         online: indicates logged into tournament (boolean)
+        """
+        self.id = uuid
+        self.user = user
+        self.currentLegend = None
+        self.online = False
+    
+    

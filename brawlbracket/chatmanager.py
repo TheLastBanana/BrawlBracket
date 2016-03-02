c2cef9ac80c49f01699b5c3116723fd0246c6967
kuzi117 <braedy@ualberta.ca>
2016-03-01 22:24:58 -0500

    Persistent chats.

--- brawlbracket/chatmanager.py
+++ brawlbracket/chatmanager.py
@@ -1,6 +1,12 @@
+import json
+
 from brawlbracket import chat
+from brawlbracket import db_wrapper
+from brawlbracket import util
 
 _chats = []
+
+_db = None
 
 def createChat():
     """
@@ -10,8 +16,10 @@ def createChat():
     """
     newChat = chat.Chat()
     
+    _writeChatToDB(newChat)
+    
     _chats.append(newChat)
-        
+    
     return newChat
     
 def getChat(id):
@@ -24,5 +32,71 @@ def getChat(id):
     for c in _chats:
         if c.id == id:
             return c
+            
+    c = _getChatFromDB(id)
+    if c is not None:
+        _chats.append(c)
+        return c
     
     return None
+
+def _getChatFromDB(id):
+    """
+    Gets a chat from the database by steamId.
+    
+    Returns chat if chat was in database
+    Returns None otherwise
+    """
+    if _db is None:
+        _initDB()
+    
+    rows = _db.select_values('chat', ['*'], ['id = {}'.format(id)])
+    
+    if rows:
+        chatData = rows[0]
+        print('Making chat from: ', chatData)
+        id = uuid.UUID(chatData[0])
+        log = json.loads(chatData[1])
+        
+        newChat = chat.Chat(id)
+        newChat.log = log
+        
+        return newChat
+    else:
+        return None
+
+def _writeChatToDB(c):
+    """
+    Serializes a chat and then inserts it into the chat table of the database.
+    """
+    if _db is None:
+        _initDB()
+    
+    # Quick function that just wraps a string in quotes
+    q = lambda x: '\'{}\''.format(x)
+    
+    chatData = (
+        q(str(c.id)),
+        q(json.dumps(c.log))
+        )
+    print('Writing chats with: ', chatData)
+    _db.insert_values('chats', [chatData])
+        
+def _initDB():
+    print('----INIT CHAT DATABASE----')
+    # Need to global because _db is not local to this context
+    global _db
+    _db = db_wrapper.DBWrapper(util.dbName, filepath=util.dbPath)
+    
+    # Make user table
+    if not _db.table_exists('chats'):
+        fieldNames = [
+            'id',
+            'log'
+            ]
+        fieldTypes = [
+            'TEXT',
+            'TEXT'
+            ]
+            
+        _db.create_table('chats', fieldNames, fieldTypes, 'id')

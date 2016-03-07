71526db1e879d8d1c8414375a3b9ca415bab7cb4
Elliot Colp <thelastbanana@gmail.com>
2016-03-06 19:02:05 -0500

    Change bracket format to make state updates and sub-brackets easier

--- brawlbracket/src/static/brawlbracket/js/bracket.jsx
+++ brawlbracket/src/static/brawlbracket/js/bracket.jsx
@@ -3,22 +3,32 @@
 var BracketNode = React.createClass({
     render: function () {
         var teamData = this.props.teams;
+        var matchData = this.props.matches;
+
+        var match = matchData[this.props.root];
+        var scores = match.scores;
+        var winner = match.winner;
 
         // Get names for each team
         var teamNames = [];
         for (var i = 0; i < 2; ++i) {
-            teamNames.push(teamData[this.props.match.teams[i]]);
+            teamNames.push(teamData[match.teams[i]]);
         }
 
         // If there are children, create a column for them
         var childColumn;
         var getTop = this.getTop;
-        if (this.props.match.children) {
+        if (match.children) {
             childColumn = (
                 <div className="bracket-column">
-                    {this.props.match.children.map(function(childMatch) {
+                    {match.children.map(function(childMatch) {
                         return (
-                            <BracketNode match={childMatch} teams={teamData} hasParent={true} key={childMatch.id} ref={'child'+childMatch.id} />
+                            <BracketNode root={childMatch}
+                                         teams={teamData}
+                                         matches={matchData}
+                                         hasParent={true}
+                                         key={childMatch}
+                                         ref={'child'+childMatch} />
                         );
                     })}
                 </div>
@@ -31,16 +41,13 @@ var BracketNode = React.createClass({
             connector = <div className="bracket-connector" ref="connector"></div>;
         }
 
-        var scores = this.props.match.scores;
-        var winner = this.props.match.winner;
-
         return (
               <div className="bracket-row">
                 {childColumn}
                 <div className="bracket-column">
                     <div className="bracket-match" ref="match">
                         <div className="bracket-match-id">
-                            <div className="bracket-match-id-circle">{this.props.match.id}</div>
+                            <div className="bracket-match-id-circle">{this.props.root + 1}</div>
                         </div>
                         <div className="bracket-match-inner">
                             {[0, 1].map(function(i) {
@@ -61,11 +68,11 @@ var BracketNode = React.createClass({
 
     // Tell children to update connectors now that DOM has rendered
     componentDidMount: function() {
-        var children = this.props.match.children;
+        var children = this.props.matches[this.props.root].children;
         if (!children) return;
 
         for (var i = 0; i < children.length; ++i) {
-            var refName = 'child' + children[i].id;
+            var refName = 'child' + children[i];
 
             if (this.refs[refName]) {
                 this.refs[refName].updateConnector(this.getTop());
@@ -103,10 +110,11 @@ var Bracket = React.createClass({
     render: function () {
         var root = this.props.bracket.root;
         var teams = this.props.bracket.teams;
+        var matches = this.props.bracket.matches;
 
         return (
             <div className="bracket">
-                <BracketNode match={root} teams={teams} />
+                <BracketNode root={root} teams={teams} matches={matches} />
             </div>
         );
     }

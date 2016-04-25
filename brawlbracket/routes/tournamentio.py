5a96c77740f3cf927592e0e31269e28d6e8a47ae
Elliot Colp <thelastbanana@gmail.com>
2016-04-24 21:29:15 -0400

    Move boilerplate into a decorator for tournamentio

--- brawlbracket/routes/tournamentio.py
+++ brawlbracket/routes/tournamentio.py
@@ -1,5 +1,9 @@
+from functools import wraps
+
 from flask import session
 from flask import request
+from flask import g
+from flask import abort
 from flask_socketio import join_room
 from flask_socketio import leave_room
 from flask_socketio import emit
@@ -10,75 +14,95 @@ from brawlbracket import tournamentmanager as tm
 
 print('Registering tournamentio routes...')
 
-@socketio.on('connect', namespace='/tournament')
-def user_connect():
-    userId = session.get('userId', None)
-    user = um.getUserById(userId)
-    tourneyId = session.get('tourneyId', None)
-    tournament = tm.getTournamentById(tourneyId)
+def require_tourney_data(f):
+    """
+    Place all the data needed for tournament namespace socketIO calls in g. The following will be set:
+    g.userId
+    g.user
+    g.tourneyId
+    g.tournament
+    g.match
+    g.team
+    g.player
     
+    An error will be emitted if any of the above values are invalid.
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        g.userId = session.get('userId', None)
+        g.user = um.getUserById(g.userId)
+        
+        # User doesn't exist
+        if g.user is None:
+            print('User doesn\'t exist; connection rejected')
+            emit('error', {'code': 'bad-participant'},
+                broadcast=False, include_self=True)
+            return False
+            
+        g.tourneyId = session.get('tourneyId', None)
+        g.tournament = tm.getTournamentById(g.tourneyId)
+            
+        if g.tournament is None:
+            # TODO: add actual error message
+            abort(404)
+            
+        info = g.tournament.getUserInfo(g.user)
+        
+        # User isn't in this tournament
+        if info is None:
+            # TODO: add actual error message
+            abort(403)
+            
+        g.match, g.team, g.player = info
+            
+        return f(*args, **kwargs)
+        
+    return decorated_function
+
+@socketio.on('connect', namespace='/tournament')
+@require_tourney_data
+def user_connect():
     # Extra data that we'll send in the handshake event
     extraData = {
-        'playerSettings': user.getSettings()
+        'playerSettings': g.user.getSettings()
     }
     
-    # User doesn't exist
-    if user is None:
-        print('User doesn\'t exist; connection rejected')
-        emit('error', {'code': 'bad-participant'},
-            broadcast=False, include_self=True)
-        return False
-    
-    #Tournament doesn't exist
-    if tournament is None:
+    if g.team.eliminated:
+        # This can be handled more gracefully
         abort(404)
     
-    info = tournament.getUserInfo(user)
+    # Affect match state
+    g.player.online += 1
     
-    if info is None:
-        # TODO: Issue error saying we somehow got into a tournament that
-        # we don't live in (should also check if the player is an admin first)
-        pass
-        
-    else:
-        match, team, player = info
-        
-        if team.eliminated:
-            # This can be handled more gracefully
-            abort(404)
-        
-        # Affect match state
-        player.online += 1
-        
-        # XXX update state, put in listener
-        match._updateState()
-        
-        # Join new room
-        session['matchId'] = match.id
-        join_room(match.id)
+    # XXX update state, put in listener
+    g.match._updateState()
     
-        # This needs to be done in the /chat namespace, so switch it temporarily
-        request.namespace = '/chat'
-        join_room(match.chat.getRoom())
-        if player.adminChat:
-            join_room(player.adminChat.getRoom())
-        request.namespace = '/tournament'
-        
-        extraData['adminChat'] = str(player.adminChat.id if player.adminChat else None)
+    # Join new room
+    session['matchId'] = g.match.id
+    join_room(g.match.id)
+
+    # This needs to be done in the /chat namespace, so switch it temporarily
+    request.namespace = '/chat'
+    join_room(g.match.chat.getRoom())
+    if g.player.adminChat:
+        join_room(g.player.adminChat.getRoom())
+    request.namespace = '/tournament'
+    
+    extraData['adminChat'] = str(g.player.adminChat.id if g.player.adminChat else None)
     
     print('Participant {} connected, joined room #{}'
-        .format(user.id, match.id))
+        .format(g.user.id, g.match.id))
     
-    lobbyData = match.lobbyData
+    lobbyData = g.match.lobbyData
     
     # Admin-only data
-    if tournament.isAdmin(user):
-        extraData['playerChats'] = [ str(p.adminChat.id) for p in tournament.players ]
+    if g.tournament.isAdmin(g.user):
+        extraData['playerChats'] = [ str(p.adminChat.id) for p in g.tournament.players ]
         
         # This needs to be done in the /chat namespace, so switch it temporarily
         # Join all players' admin chats
         request.namespace = '/chat'
-        for p in tournament.players:
+        for p in g.tournament.players:
             if p.adminChat:
                 join_room(p.adminChat.getRoom())
         request.namespace = '/tournament'
@@ -98,254 +122,109 @@ def user_connect():
     updatedLobbyData['state'] = lobbyData['state']
     
     emit('update lobby', updatedLobbyData, broadcast=True, include_self=False,
-            room = match.id, namespace='/tournament')
+            room = g.match.id, namespace='/tournament')
     
 @socketio.on('disconnect', namespace='/tournament')
+@require_tourney_data
 def user_disconnect():
-    userId = session.get('userId', None)
-    tournamentId = session.get('tourneyId', None)
-    
-    user = um.getUserById(userId)
-    
-    # User doesn't exist, this shouldn't happen ever
-    if user is None:
-        raise AssertionError('User disconnected, but didn\'t exist')
-        
-    tournament = tm.getTournamentById(tournamentId)
-    if tournament is None:
-        abort(404)
-    
-    # Tournament doesn't exist
-    if tournament is None:
-        return
-    
-    info = tournament.getUserInfo(user)
-    
-    if info is None:
-        # TODO: Issue error saying we somehow got into a tournament that
-        # we don't live in
-        pass
-    
-    match, team, player = info
-    
     # Affect state
-    player.online -= 1
+    g.player.online -= 1
     
-    if match is not None:
+    if g.match is not None:
         # XXX update state, put in listener
-        match._updateState()
+        g.match._updateState()
     # Match is none, player was eliminated
     else:
         return
     
-    lobbyData = match.lobbyData
+    lobbyData = g.match.lobbyData
     updatedLobbyData = {}
     updatedLobbyData['teams'] = lobbyData['teams']
     updatedLobbyData['state'] = lobbyData['state']
     
     emit('update lobby', updatedLobbyData, broadcast=True, include_self=False,
-            room = match.id)
+            room = g.match.id)
     
-    print('User {} disconnected'.format(user.id))
+    print('User {} disconnected'.format(g.user.id))
     
 # State reporting from client
 
 # Someone picked their legend
 @socketio.on('pick legend', namespace='/tournament')
+@require_tourney_data
 def pick_legend(data):
-    userId = session.get('userId', None)
-    user = um.getUserById(userId)
-    tourneyId = session.get('tourneyId', None)
-    tournament = tm.getTournamentById(tourneyId)
+    g.player.currentLegend = data['legendId']
+    g.match._updateState()
     
-    # User doesn't exist
-    if user is None:
-        print('User doesn\'t exist; connection rejected')
-        emit('error', {'code': 'bad-participant'},
-            broadcast=False, include_self=True)
-        return False
-    
-    #Tournament doesn't exist
-    if tournament is None:
-        abort(404)
-    
-    info = tournament.getUserInfo(user)
-    
-    if info is None:
-        # TODO: Issue error saying we somehow got into a tournament that
-        # we don't live in
-        pass
-    
-    match, team, player = info
-    
-    player.currentLegend = data['legendId']
-    match._updateState()
-    
-    lobbyData = match.lobbyData
+    lobbyData = g.match.lobbyData
     updatedLobbyData = {}
     updatedLobbyData['state'] = lobbyData['state']
     updatedLobbyData['teams'] = lobbyData['teams']
     
     emit('update lobby', updatedLobbyData, broadcast=True, include_self=True,
-            room = match.id)
+            room = g.match.id)
     
-    print('User {} selected {}'.format(user.id, data['legendId']))
+    print('User {} selected {}'.format(g.user.id, data['legendId']))
     
 # Someone banned a realm
 @socketio.on('ban realm', namespace='/tournament')
+@require_tourney_data
 def ban_realm(data):
-    userId = session.get('userId', None)
-    user = um.getUserById(userId)
-    tourneyId = session.get('tourneyId', None)
-    tournament = tm.getTournamentById(tourneyId)
+    g.match.addRealmBan(data['realmId'])
+    g.match._updateState()
     
-    # User doesn't exist
-    if user is None:
-        print('User doesn\'t exist; connection rejected')
-        emit('error', {'code': 'bad-participant'},
-            broadcast=False, include_self=True)
-        return False
-    
-    #Tournament doesn't exist
-    if tournament is None:
-        abort(404)
-    
-    info = tournament.getUserInfo(user)
-    
-    if info is None:
-        # TODO: Issue error saying we somehow got into a tournament that
-        # we don't live in
-        pass
-    
-    match, team, player = info
-    
-    match.addRealmBan(data['realmId'])
-    match._updateState()
-    
-    lobbyData = match.lobbyData
+    lobbyData = g.match.lobbyData
     updatedLobbyData = {}
     updatedLobbyData['state'] = lobbyData['state']
     updatedLobbyData['realmBans'] = lobbyData['realmBans']
     updatedLobbyData['currentRealm'] = lobbyData['currentRealm']
     
     emit('update lobby', updatedLobbyData, broadcast=True, include_self=True,
-            room = match.id)
+            room = g.match.id)
     print('Banned {}'.format(data['realmId']))
     
 # Someone picked a realm
 @socketio.on('pick realm', namespace='/tournament')
+@require_tourney_data
 def pick_realm(data):
-    userId = session.get('userId', None)
-    user = um.getUserById(userId)
-    tourneyId = session.get('tourneyId', None)
-    tournament = tm.getTournamentById(tourneyId)
+    g.match.currentRealm = data['realmId']
+    g.match._updateState()
     
-    # User doesn't exist
-    if user is None:
-        print('User doesn\'t exist; connection rejected')
-        emit('error', {'code': 'bad-participant'},
-            broadcast=False, include_self=True)
-        return False
-    
-    #Tournament doesn't exist
-    if tournament is None:
-        abort(404)
-    
-    info = tournament.getUserInfo(user)
-    
-    if info is None:
-        # TODO: Issue error saying we somehow got into a tournament that
-        # we don't live in
-        pass
-    
-    match, team, player = info
-    
-    match.currentRealm = data['realmId']
-    match._updateState()
-    
-    lobbyData = match.lobbyData
+    lobbyData = g.match.lobbyData
     updatedLobbyData = {}
     updatedLobbyData['state'] = lobbyData['state']
     updatedLobbyData['currentRealm'] = lobbyData['currentRealm']
     
     emit('update lobby', updatedLobbyData, broadcast=True, include_self=True,
-            room = match.id)
+            room = g.match.id)
     print('Picked {}'.format(data['realmId']))
 
 # A room was selected
 @socketio.on('set room', namespace='/tournament')
+@require_tourney_data
 def select_room(data):
-    userId = session.get('userId', None)
-    user = um.getUserById(userId)
-    tourneyId = session.get('tourneyId', None)
-    tournament = tm.getTournamentById(tourneyId)
+    g.match.roomNumber = data['roomNumber']
+    g.match._updateState()
     
-    # User doesn't exist
-    if user is None:
-        print('User doesn\'t exist; connection rejected')
-        emit('error', {'code': 'bad-participant'},
-            broadcast=False, include_self=True)
-        return False
-    
-    #Tournament doesn't exist
-    if tournament is None:
-        abort(404)
-    
-    info = tournament.getUserInfo(user)
-    
-    if info is None:
-        # TODO: Issue error saying we somehow got into a tournament that
-        # we don't live in
-        pass
-    
-    match, team, player = info
-    
-    match.roomNumber = data['roomNumber']
-    match._updateState()
-    
-    lobbyData = match.lobbyData
+    lobbyData = g.match.lobbyData
     updatedLobbyData = {}
     updatedLobbyData['state'] = lobbyData['state']
     updatedLobbyData['roomNumber'] = lobbyData['roomNumber']
     
     emit('update lobby', updatedLobbyData, broadcast=True, include_self=True,
-            room = match.id)
+            room = g.match.id)
     print('Selected room number {}'.format(data['roomNumber']))
     
     
 # A team win was reported
 @socketio.on('report win', namespace='/tournament')
+@require_tourney_data
 def report_win(data):
-    userId = session.get('userId', None)
-    user = um.getUserById(userId)
-    tourneyId = session.get('tourneyId', None)
-    tournament = tm.getTournamentById(tourneyId)
+    g.match.incrementScore(data['teamIndex'])
     
-    # User doesn't exist
-    if user is None:
-        print('User doesn\'t exist; connection rejected')
-        emit('error', {'code': 'bad-participant'},
-            broadcast=False, include_self=True)
-        return False
+    g.match._updateState()
     
-    #Tournament doesn't exist
-    if tournament is None:
-        abort(404)
-    
-    info = tournament.getUserInfo(user)
-    
-    if info is None:
-        # TODO: Issue error saying we somehow got into a tournament that
-        # we don't live in
-        pass
-    
-    match, team, player = info
-    
-    match.incrementScore(data['teamIndex'])
-    
-    match._updateState()
-    
-    lobbyData = match.lobbyData
+    lobbyData = g.match.lobbyData
     updatedLobbyData = {}
     updatedLobbyData['state'] = lobbyData['state']
     updatedLobbyData['teams'] = lobbyData['teams']
@@ -353,54 +232,30 @@ def report_win(data):
     updatedLobbyData['realmBans'] = lobbyData['realmBans']
     
     emit('update lobby', updatedLobbyData, broadcast=True, include_self=True,
-            room = match.id)
+            room = g.match.id)
     
-    if match.winner is not None and match.nextMatch is not None:
-        nextLobbyData = match.nextMatch.lobbyData
+    if g.match.winner is not None and g.match.nextMatch is not None:
+        nextLobbyData = g.match.nextMatch.lobbyData
         updatedNextLobbyData = {}
         updatedNextLobbyData['state'] = nextLobbyData['state']
         updatedNextLobbyData['teams'] = nextLobbyData['teams']
         emit('update lobby', updatedNextLobbyData, broadcast=True, 
-                include_self=False, room = match.nextMatch.id)
+                include_self=False, room = g.match.nextMatch.id)
 
 # A player is ready to advance to the next match
 @socketio.on('advance lobby', namespace='/tournament')
+@require_tourney_data
 def advance_lobby():
-    userId = session.get('userId', None)
-    user = um.getUserById(userId)
-    tourneyId = session.get('tourneyId', None)
-    tournament = tm.getTournamentById(tourneyId)
-    
-    # User doesn't exist
-    if user is None:
-        print('User doesn\'t exist; connection rejected')
-        emit('error', {'code': 'bad-participant'},
-            broadcast=False, include_self=True)
-        return False
-    
-    #Tournament doesn't exist
-    if tournament is None:
-        abort(404)
-    
-    info = tournament.getUserInfo(user)
-    
-    if info is None:
-        # TODO: Issue error saying we somehow got into a tournament that
-        # we don't live in
-        pass
-    
-    match, team, player = info
-    
-    if team.eliminated:
+    if g.team.eliminated:
         raise AssertionError('Eliminated player trying to advance match.')
     
-    for m in match.prereqMatches:
+    for m in g.match.prereqMatches:
         if m is None:
             continue
         for t in m.teams:
             # This user's team in a previous match, we want to leave rooms
             # associated with this match
-            if t.id == team.id:
+            if t.id == g.team.id:
                 leave_room(m.id)
                 
                 # This needs to be done in the /chat namespace, so switch it temporarily
@@ -418,12 +273,12 @@ def advance_lobby():
         raise AssertionError('Didn\'t find rooms to leave.')
     
     # Join new match lobby
-    join_room(match.id)
+    join_room(g.match.id)
     
     # This needs to be done in the /chat namespace, so switch it temporarily
     request.namespace = '/chat'
-    join_room(match.chat.getRoom())
+    join_room(g.match.chat.getRoom())
     request.namespace = '/tournament'
     
-    emit('join lobby', {'lobbyData': match.lobbyData},
-            broadcast=False, include_self=True, room = match.id)
+    emit('join lobby', {'lobbyData': g.match.lobbyData},
+            broadcast=False, include_self=True, room = g.match.id)
